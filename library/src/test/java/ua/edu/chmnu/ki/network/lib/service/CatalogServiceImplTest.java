77a6865e3e256cf5cd4aa7dba98e1c3276c507e1
Sergii Puzyrov <sergii.puzyrov@gmail.com>
2024-11-06 11:50:25 +0200

    Added example of unit tests

---
+++ library/src/test/java/ua/edu/chmnu/ki/network/lib/service/CatalogServiceImplTest.java
@@ -0,0 +1,68 @@
+package ua.edu.chmnu.ki.network.lib.service;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import ua.edu.chmnu.ki.network.lib.error.CatalogException;
+import ua.edu.chmnu.ki.network.lib.mapper.CatalogMapper;
+import ua.edu.chmnu.ki.network.lib.persistence.entity.Catalog;
+import ua.edu.chmnu.ki.network.lib.persistence.repository.CatalogRepository;
+import ua.edu.chmnu.ki.network.lib.web.dto.CatalogDTO;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class CatalogServiceImplTest {
+
+    @Mock
+    private CatalogRepository catalogRepository;
+
+    @Mock
+    private CatalogMapper catalogMapper;
+
+    @InjectMocks
+    private CatalogServiceImpl service;
+
+    @Mock
+    private Catalog catalog;
+
+    @Mock
+    private CatalogDTO catalogDTO;
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 3})
+    void shouldSuccessGetItemById(Integer id) {
+        when(catalogRepository.findById(id)).thenReturn(Optional.of(catalog));
+        when(catalogMapper.mapTo(catalog)).thenReturn(catalogDTO);
+
+        assertEquals(catalogDTO, service.getById(id));
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 3})
+    void shouldFailGetItemByIdWhenItIsNotExist(Integer id) {
+        when(catalogRepository.findById(id)).thenReturn(Optional.empty());
+
+        assertThrows(CatalogException.class, () -> service.getById(id));
+    }
+
+    @Test
+    void shouldGetAllCatalogs() {
+        when(catalogRepository.findAll()).thenReturn(List.of(catalog));
+        when(catalogMapper.mapTo(catalog)).thenReturn(catalogDTO);
+
+        var result = service.getAll();
+
+        assertEquals(1, result.size());
+        assertEquals(catalogDTO, result.getFirst());
+    }
+}
