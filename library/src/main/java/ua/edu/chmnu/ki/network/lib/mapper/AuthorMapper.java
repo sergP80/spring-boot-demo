57543d704bd79f318c087275775ae447296f04bd
Sergii Puzyrov <sergii.puzyrov@gmail.com>
2024-11-13 10:58:48 +0200

    Mapstruct warn for unknown

--- library/src/main/java/ua/edu/chmnu/ki/network/lib/mapper/AuthorMapper.java
+++ library/src/main/java/ua/edu/chmnu/ki/network/lib/mapper/AuthorMapper.java
@@ -4,10 +4,11 @@ import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
 import org.mapstruct.Mappings;
+import org.mapstruct.ReportingPolicy;
 import ua.edu.chmnu.ki.network.lib.persistence.entity.Author;
 import ua.edu.chmnu.ki.network.lib.web.dto.AuthorDTO;
 
-@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING, unmappedTargetPolicy = ReportingPolicy.WARN)
 public interface AuthorMapper {
 
     @Mappings({
--- library/src/main/java/ua/edu/chmnu/ki/network/lib/mapper/CatalogMapper.java
+++ library/src/main/java/ua/edu/chmnu/ki/network/lib/mapper/CatalogMapper.java
@@ -2,10 +2,11 @@ package ua.edu.chmnu.ki.network.lib.mapper;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingConstants;
+import org.mapstruct.ReportingPolicy;
 import ua.edu.chmnu.ki.network.lib.persistence.entity.Catalog;
 import ua.edu.chmnu.ki.network.lib.web.dto.CatalogDTO;
 
-@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING, unmappedTargetPolicy = ReportingPolicy.WARN)
 public interface CatalogMapper {
 
     Catalog mapTo(CatalogDTO source);
